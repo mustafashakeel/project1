f946c950f4b72067377b4915ea189196e55be187
mustafa queshi <mustafa queshi>
2016-10-27 13:47:40 -0700

    First Commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+require.config({
+	paths:{
+		'jquery':'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery'
+	}
+})
+
+require(['main'],function(){});
---
+++ js/main.js
@@ -0,0 +1,6 @@
+define(['jquery'],function($){
+
+var aa = $('#testing');
+console.log(aa);
+
+});
